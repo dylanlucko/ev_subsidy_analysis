42ec62a5d575d106420a4c28357a7d4eae182f7f
dylanlucko <dylanlucko@gwu.edu>
2025-02-08 23:45:27 -0500

    compute treatment and control at zip level, haversine

---
+++ Data Preprocessing Code/function to relate zip to lat long.R
@@ -0,0 +1,51 @@
+
+
+ev_subsidies_per_day_2010_2023 <- read_excel("~/GitHub/ev_subsidy_analysis/Datasets Pre Panel Creation/ev_subsidies_per_day_2010_2023.xlsx")
+
+length(unique(ev_subsidies_per_day_2010_2023$County))
+#58
+
+length(unique(ev_subsidies_per_day_2010_2023$ZIP))
+#1873
+
+# not the same granularity. 
+
+colnames(ev_subsidies_per_day_2010_2023)
+
+
+#####
+#####
+##### Relate Zip code to range of lat long for comparison with lat-long-site metric
+#####
+#####
+
+
+# Install package if needed
+install.packages("zipcodeR")
+
+# Load the package
+library(zipcodeR)
+
+
+# Get unique ZIP codes from ev_subsidies_per_day_2010_2023
+zip_list <- unique(ev_subsidies_per_day_2010_2023$ZIP)
+
+# Retrieve lat/long for all ZIPs
+zip_data <- lapply(zip_list, reverse_zipcode) %>% bind_rows()
+
+head(zip_data)  # Now we have ZIP, lat, and long
+
+
+##### Merge back into ev subsidy data
+
+# Ensure ZIP codes are formatted as characters
+ev_subsidies_per_day_2010_2023 <- ev_subsidies_per_day_2010_2023 %>%
+  mutate(ZIP = as.character(ZIP))
+
+zip_data <- zip_data %>%
+  select(zipcode, lat, lng) %>%
+  rename(zip_lat = lat, zip_long = lng, ZIP = zipcode)
+
+# Merge ZIP lat/long into the EV subsidies dataset
+ev_subsidies_per_day_2010_2023 <- ev_subsidies_per_day_2010_2023 %>%
+  left_join(zip_data, by = "ZIP")
