671a1e3e82af5d4deb13fbc65ede4a8035727b98
dylanlucko <dylanlucko@gwu.edu>
2025-02-09 22:59:17 -0500

    updates to DiD analysis

--- Modeling Code/difference_in_differences_v1.R
+++ Modeling Code/difference_in_differences_v1.R
@@ -10,3 +10,72 @@ panel_data_did <- panel_data_did %>%
 
 colnames(panel_data_did)
 
+#####
+#####
+##### Create Post var #####
+#####
+#####
+
+
+# Ensure `date` is in Date format
+panel_data_did <- panel_data_did %>%
+  mutate(date = as.Date(date, format = "%Y-%m-%d"))
+
+
+# Create Post column based on date
+panel_data_did <- panel_data_did %>%
+  mutate(Post = ifelse(year(date) >= 2011, 1, 0))
+
+
+#####
+#####
+##### DiD Model #####
+#####
+#####
+
+library(fixest)  # For fixed effects regression
+
+# Ensure `date` is in Date format
+panel_data_did <- panel_data_did %>%
+  mutate(date = as.Date(date, format = "%Y-%m-%d")) %>%
+  filter(year(date) >= 2010)  # Keep only data from 2010 onwards
+
+panel_data_did <- panel_data_did %>%
+  mutate(month = month(date, label = TRUE, abbr = TRUE))
+
+
+# Run the Difference-in-Differences (DiD) model
+did_model <- feols(
+  no2_ppb ~ Treatment_zip * Post + log(population) + log(income_per_capita) + total_fertilizer +  chemical_insecticide + num_gasoline_cars | cbsa_code + year + month,  # Fixed effects
+  data = panel_data_did, 
+  cluster = "county"  # Cluster SEs at site level
+)
+
+# View results
+summary(did_model)
+
+summary(did_model, se = "hetero")  # Heteroskedasticity-robust
+
+
+# Count NA values per column
+colSums(is.na(panel_data_did))
+
+
+#####
+#####
+##### Model 2 #####
+#####
+#####
+
+
+# Run the Difference-in-Differences (DiD) model
+did_model <- feols(
+  no2_ppb ~ Treatment_zip * Post + (population) + (income_per_capita) + total_fertilizer +  chemical_insecticide + total_chemical_fungicide + total_chemical_herbicide + num_gasoline_cars | cbsa_code + year + month,  # Fixed effects
+  data = panel_data_did, 
+  cluster = "county"  # Cluster SEs at site level
+)
+
+# View results
+summary(did_model)
+
+summary(did_model, se = "hetero")
