4f251551718255adb4072b0bccb41372e508d6de
dylanlucko <dylanlucko@gwu.edu>
2025-02-12 02:04:21 -0500

    new EV sales data

---
+++ Data Preprocessing Code/zev sales.R
@@ -0,0 +1,5 @@
+
+
+New_ZEV_Sales_Last_updated_01_31_2025_ada <- read_excel("~/GitHub/ev_subsidy_analysis/Datasets Pre Panel Creation/New_ZEV_Sales_Last_updated_01-31-2025_ada.xlsx", sheet = "ZIP")
+
+colnames(New_ZEV_Sales_Last_updated_01_31_2025_ada)
